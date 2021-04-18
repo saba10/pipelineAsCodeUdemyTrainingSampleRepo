ababdf5d501b1b52b8e3451921b7795a7b072dc1
M <sabm@lowes.com>
2021-04-19 00:04:43 +0530

    testing-when->changeset

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,10 +6,11 @@ pipeline{
 	    when{
 	        //branch 'dev'                                   
 		//tag "release*"                     
-		//buildingTag()                                    //true on all tags
-		changelog '.*test*'                               //true when commit message matches the expression provided 
-		//changeRequest()                                //true on all pull requests	
-		//changeRequest title:'test-when*'                 //true based on the pull request parameters
+		//buildingTag()                                        //true on all tags
+		changelog '.*test*'                                    //true when commit message matches the expression provided 
+		//changeRequest()                                      //true on all pull requests	
+		//changeRequest title:'test-when*'                     //true based on the pull request parameters
+		changeset pattern:'**/*.groovy', caseSensitive: true   //true when changes are made to one or more filenames matching the pattern
             }
 	    steps{
 	        echo "testing when"
